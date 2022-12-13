9aacb6865ea567c76d6e1f03bd2fe0b7f49e69d8
Yaroslav Onofriichuk <onofriichukyaroslav@gmail.com>
2022-12-13 13:44:38 +0500

    add python labs

---
+++ Python programming language/Lab_15/lab_15.py
@@ -0,0 +1,31 @@
+import math
+
+
+def func_f(x):
+    return math.pow(math.e, math.sin(x)) - x
+
+
+def func_g(x):
+    return math.log(1 + math.sqrt(x)) - math.cos(x)
+
+
+def func_a_ij(i):
+    return math.fabs(func_f(i) + func_g(i))
+
+
+def func_n_a(m, n):
+    m = min(m, n)
+    max_value = float("-inf")
+
+    for i in range(1, m+1):
+        max_value = max(max_value, func_a_ij(i))
+
+    return math.sqrt(max_value)
+
+
+if __name__ == "__main__":
+    m = int(input("Введите m: "))
+    n = int(input("Введите n: "))
+
+    print(func_n_a(m, n))
+
