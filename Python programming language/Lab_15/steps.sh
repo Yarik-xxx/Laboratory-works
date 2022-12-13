96d4e4caa1d239ca2f870c9965020a7b1673fed4
Yarik_dev <99239824+Yarik-xxx@users.noreply.github.com>
2022-12-13 15:20:22 +0500

    Add files via upload

---
+++ Python programming language/Lab_15/steps.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+
+time python3 task_1.py
+time ./task
+time ./task_1.bin
+time python3 task_4.py
+
+
+# gcc -Wall task_2.c -o task -lm
+# python3 -m nuitka task_1.py
+# gcc task_4_lib_c.c -fPIC -shared -o c_shared_lib.so -lm -Ofast -march=native
