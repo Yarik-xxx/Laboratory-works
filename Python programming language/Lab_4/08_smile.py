fd0973d87f6d9985a940ef417001166a0e0296fc
Yaroslav Onofriichuk <onofriichukyaroslav@gmail.com>
2023-01-05 18:11:01 +0500

    finish: added wording of tasks, removed unnecessary files

--- Python programming language/Lab_4/08_smile.py
+++ Python programming language/Lab_4/08_smile.py
@@ -14,20 +14,20 @@ def print_smile(x, y, color):
     position = sd.get_point(x, y)
     sd.circle(position, radius=50, color=color, width=2)
     #sd.line()
-    sd.line(sd.get_point(position.x - 30, position.y - 20), sd.get_point(position.x - 10, position.y - 30), color=color,
+    sd.line(sd.get_point(position.max_div - 30, position.y - 20), sd.get_point(position.max_div - 10, position.y - 30), color=color,
             width=2)
-    sd.line(sd.get_point(position.x - 10, position.y - 30), sd.get_point(position.x + 10, position.y - 30), color=color,
+    sd.line(sd.get_point(position.max_div - 10, position.y - 30), sd.get_point(position.max_div + 10, position.y - 30), color=color,
             width=2)
-    sd.line(sd.get_point(position.x + 10, position.y - 30), sd.get_point(position.x + 30, position.y - 20), color=color,
+    sd.line(sd.get_point(position.max_div + 10, position.y - 30), sd.get_point(position.max_div + 30, position.y - 20), color=color,
             width=2)
 
-    sd.line(sd.get_point(position.x - 20, position.y + 20), sd.get_point(position.x - 15, position.y + 20), color=color,
+    sd.line(sd.get_point(position.max_div - 20, position.y + 20), sd.get_point(position.max_div - 15, position.y + 20), color=color,
             width=5)
-    sd.line(sd.get_point(position.x + 20, position.y + 20), sd.get_point(position.x + 15, position.y + 20), color=color,
+    sd.line(sd.get_point(position.max_div + 20, position.y + 20), sd.get_point(position.max_div + 15, position.y + 20), color=color,
             width=5)
 
 
 for i in range(10):
-    print_smile(sd.random_point().x, sd.random_point().y, sd.random_color())
+    print_smile(sd.random_point().max_div, sd.random_point().y, sd.random_color())
 
 sd.pause()
